bcf88b472de6eb16c698c57d178af34583a1d89b
Leonardo <leotorresval92@gmail.com>
2020-11-20 17:24:57 -0500

    Herencia

---
+++ UC_Test/src/uc_test/ChlidClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uc_test;
+
+/**
+ *
+ * @author Develop
+ */
+public class ChlidClass extends ParentClass{
+    
+    public void Print(){
+        System.out.println(valor2);
+    }
+}
---
+++ UC_Test/src/uc_test/ParentClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uc_test;
+
+/**
+ *
+ * @author Develop
+ */
+public class ParentClass {
+    String valor ;
+    String valor2;
+}
